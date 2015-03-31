b7806078cc51c8005be2e7e8b72990d460bbfe28
Yuri Artemev <i@artemeff.com>
2015-03-31 11:21:47 +0300

    fix request class

--- Transee/API/Request.cs
+++ Transee/API/Request.cs
@@ -4,28 +4,28 @@ using System.Threading.Tasks;
 using Transee.Common;
 
 namespace Transee.API {
-    class Request {
-        private string host = "https://transee.in/api/v1/";
+    class Request : IDisposable {
+        private string host = "http://192.168.1.219:8000/api/v1/";
         private HttpClient client = new HttpClient();
         private Cache cache = new Cache();
 
         public async Task<string> GetCity(string city, string method) {
-            var url = String.Concat(host, "cities/", city, "/", method);
+            var url = string.Concat(host, "cities/", city, "/", method);
             return await this.Fetch(url);
         }
 
         public async Task<string> GetCity(string city) {
-            var url = String.Concat(host, "cities/", city);
+            var url = string.Concat(host, "cities/", city);
             return await this.Fetch(url);
         }
 
         public async Task<string> Get(string method) {
-            var url = String.Concat(host, method);
+            var url = string.Concat(host, method);
             return await this.Fetch(url);
         }
 
         public async Task<string> Post(string city, string method, FormUrlEncodedContent body) {
-            var url = String.Concat(host, "cities/", city, "/", method);
+            var url = string.Concat(host, "cities/", city, "/", method);
             var response = await client.PostAsync(url, body);
             var responseString = await response.Content.ReadAsStringAsync();
             return responseString;
@@ -42,6 +42,17 @@ namespace Transee.API {
                 cache.Set(url, responseString);
                 return responseString;
             }
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                client.Dispose();
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
