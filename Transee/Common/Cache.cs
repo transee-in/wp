24849d4d28cdc262d2a652e1e0d85ac9cb6ff32d
Yuri Artemev <i@artemeff.com>
2015-04-08 12:14:59 +0300

    improve cache

--- Transee/Common/Cache.cs
+++ Transee/Common/Cache.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,15 @@ using Windows.Storage.Streams;
 
 namespace Transee.Common {
     class Cache {
+        private static Dictionary<string, string> runtimeCache = new Dictionary<string, string>();
+
         public async void Set(string key, string data) {
+            Debug.WriteLine("[Cache] set {0}", key);
+
+            if (runtimeCache.ContainsKey(key)) {
+                return;
+            }
+
             try {
                 string fileName = CreateKey(key);
                 IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
@@ -18,27 +28,36 @@ namespace Transee.Common {
                     byte[] content = Encoding.UTF8.GetBytes(data);
                     await stream.WriteAsync(content, 0, content.Length);
                     stream.Dispose();
+                    runtimeCache.Add(key, data);
                 }
             } catch (Exception ex) {
                 throw ex;
             }
         }
 
-        public async Task<String> Get(string key) {
+        public async Task<string> Get(string key) {
+            if (runtimeCache.ContainsKey(key)) {
+                Debug.WriteLine("[Cache] get from runtime {0}", key);
+                return runtimeCache[key];
+            }
+
             try {
+                Debug.WriteLine("[Cache] get from filesystem {0}", key);
                 string fileName = CreateKey(key);
                 IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
                 IStorageFile storageFile = await applicationFolder.GetFileAsync(fileName);
                 IRandomAccessStream accessStream = await storageFile.OpenReadAsync();
-                string text = null;
+                string data = null;
                 using (Stream stream = accessStream.AsStreamForRead((int) accessStream.Size)) {
                     byte[] content = new byte[stream.Length];
                     await stream.ReadAsync(content, 0, (int) stream.Length);
-                    text = Encoding.UTF8.GetString(content, 0, content.Length);
+                    data = Encoding.UTF8.GetString(content, 0, content.Length);
                     stream.Dispose();
                 }
-                return text;
-            } catch (Exception) {
+                runtimeCache.Add(key, data);
+                return data;
+            } catch (Exception e) {
+                Debug.WriteLine("[Cache] get exception {0}", e.Message);
                 return null;
             }
         }
